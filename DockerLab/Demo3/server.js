342aa5b0d6b6d60ef4f2778ab09bbed30c61e2dc
ihpar <ismail.hakki.parlak@gmail.com>
2023-04-17 23:28:41 +0300

    adds multi app demo ex

--- DockerLab/Demo3/server.js
+++ DockerLab/Demo3/server.js
@@ -1,9 +1,41 @@
 const express = require("express");
+const axios = require("axios");
 
 const app = express();
+app.use(require("body-parser").urlencoded({ extended: false }));
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("<h1>Bu benim sayfam</h1>");
+  res.render("home");
+});
+
+app.post("/kaydet", (req, res) => {
+  const data = req.body.veri;
+
+  axios.post("http://localhost:3050/insert", {
+    db_veri: data
+  })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((err) => {
+      console.log("err:", err);
+    });
+
+  res.redirect("/");
+});
+
+app.get("/kayitlar", (req, res) => {
+  axios.get("http://localhost:3050/db")
+    .then((response) => {
+      console.log(response.data);
+      res.render("db-contents", {
+        records: response.data.split(",")
+      });
+    })
+    .catch((err) => {
+      console.log("err:", err);
+    });
 });
 
 app.listen(3000, () => {
