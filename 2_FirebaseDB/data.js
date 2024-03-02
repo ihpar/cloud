052a49759d61dbbc4fc937571c13edc5c943e9b7
ihpar <ismail.hakki.parlak@gmail.com>
2024-03-02 21:20:44 +0300

    completes fb db lab

--- 2_FirebaseDB/data.js
+++ 2_FirebaseDB/data.js
@@ -3,6 +3,10 @@ import {
   ref,
   get,
   set,
+  child,
+  push,
+  update,
+  onValue,
 } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js";
 
-export { getDatabase, ref, get, set };
+export { getDatabase, ref, get, set, child, push, update, onValue };
