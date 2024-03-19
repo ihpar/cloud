3d192e03b8adad2bf0fc170dd4c3bf7cb78c63f0
ihpar <ismail.hakki.parlak@gmail.com>
2024-03-19 13:55:42 +0300

    updates docker demo1

--- 4_DockerLab/Demo1/server.js
+++ 4_DockerLab/Demo1/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 
 const app = express();
+let i = 0;
 
 app.get("/", (req, res) => {
   res.send("<h1>Bu benim sayfam</h1>");
+  console.log(`Root gönderildi ${i}`);
+  i++;
 });
 
 app.listen(3000, () => {
